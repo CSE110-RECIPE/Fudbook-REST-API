39791ee4c4dbc2179c1d62f9d99e8865fec97fbc
Arvin Lin <a7lin@ucsd.edu>
2020-04-28 00:47:28 -0700

    Reconfigure environment variables and added new start command

--- admin_script.sh
+++ admin_script.sh
@@ -10,6 +10,13 @@ elif [ "$1" = "development" ]
 then
     echo "Development mode"
 
-    PORT1=3001 node ./src/services/recipe_service.js & \
-        PORT1=3001 PORT=3000 nodemon
+    P0=3000
+    P1=3001
+    P2=3002
+    P3=3003
+
+    PORT1=$P1 node ./src/services/recipe_service.js & \
+        PORT2=$P2 node ./src/services/book_service.js & \
+        PORT3=$P3 node ./src/services/category_service.js & \
+        PORT=$P0 PORT1=$P1 PORT2=$P2 PORT3=$P3 nodemon
 fi
