c1b9c143bb77f855ab64be270b5faf2f378a0939
Arvin Lin <a7lin@ucsd.edu>
2020-04-30 21:21:07 -0700

    Extracted services api call to a separate function

---
+++ src/js/request.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+const service = async (options, port, res, route = "" ) => {
+    try {
+        const serviceRes = await fetch(`http://localhost:${port}/${route}`,
+                                    options)
+                                .then(fres => fres.json());
+        
+        res.send(JSON.stringify(serviceRes));
+    } catch(error) {
+        res.end(error.message);
+    }
+};
+
+module.exports = {
+    service: service
+};
