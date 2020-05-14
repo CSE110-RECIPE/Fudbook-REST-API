1df7511ff17bbb98bd06e6851e285ea6dc4361f7
Peter Stratton <peterstratton121@gmail.com>
2020-05-14 16:16:45 -0700

    testers

--- test/js/recipeFilterTest.js
+++ test/js/recipeFilterTest.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 describe('Include test', () => {
-    it('0 item test', async() => {
+    it('0 include filter test', async() => {
         const options = {
             method: 'get',
             baseURL: 'http://localhost:3000/',
@@ -16,10 +16,10 @@ describe('Include test', () => {
         const res = await axios(options);
 
         // Check size
-        expect(Object.keys(res.data).length).toBe(0);
+        expect(Object.keys(res.data).length).toBe(39);
     });
 
-    it('1 item test', async() => {
+    it('1 include filter test', async() => {
 
         const options = {
             method: 'get',
@@ -38,7 +38,7 @@ describe('Include test', () => {
         expect(Object.keys(res.data).length).toBe(3);
     });
 
-    it('2 item test', async() => {
+    it('2 include filter test', async() => {
         const options = {
             method: 'get',
             baseURL: 'http://localhost:3000/',
@@ -53,10 +53,10 @@ describe('Include test', () => {
         const res = await axios(options);
 
         // Check size
-        expect(Object.keys(res.data).length).toBe(8);
+        expect(Object.keys(res.data).length).toBe(1);
     });
 
-    it('3 item test', async() => {
+    it('3 include filter test', async() => {
         const options = {
             method: 'get',
             baseURL: 'http://localhost:3000/',
@@ -64,27 +64,155 @@ describe('Include test', () => {
             headers: {'Content-Type': 'application/json'},
             data: {
                 exclude_filter: [],
-                include_filter: ["bread","butter","lamb"]
+                include_filter: ["asparagus","chicken","garlic"]
             }
         };
 
         const res = await axios(options);
 
         // Check size
-        expect(Object.keys(res.data).length).toBe(9);
+        expect(Object.keys(res.data).length).toBe(1);
     });
-});
 
-describe("Include Exclude Test", () => {
-    it("1 Include 1 Exclude Test", async() => {
+    it('Invalid filter test', async() => {
         const options = {
             method: 'get',
             baseURL: 'http://localhost:3000/',
             url: '/recipe/filter',
             headers: {'Content-Type': 'application/json'},
             data: {
-                exclude_filter: ["cheese"],
+                exclude_filter: [],
+                include_filter: ["bread","chicken","garlic", "tomato"]
+            }
+        };
+
+        const res = await axios(options);
+
+        // Check size
+        expect(Object.keys(res.data).length).toBe(0);
+    });
+});
+
+describe("Exclude Test", () => {
+    it("1 exclude filter test", async() => {
+        const options = {
+            method: 'get',
+            baseURL: 'http://localhost:3000/',
+            url: '/recipe/filter',
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                exclude_filter: ["soy"],
+                include_filter: []
+            }
+        };
+
+        const res = await axios(options);
+
+        // Check size
+        expect(Object.keys(res.data).length).toBe(34);
+    })
+
+    it("2 exclude filter test", async() => {
+        const options = {
+            method: 'get',
+            baseURL: 'http://localhost:3000/',
+            url: '/recipe/filter',
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                exclude_filter: ["soy", "chicken"],
+                include_filter: []
+            }
+        };
+
+        const res = await axios(options);
+
+        // Check size
+        expect(Object.keys(res.data).length).toBe(32);
+    })
+
+    it("3 exclude filter test", async() => {
+        const options = {
+            method: 'get',
+            baseURL: 'http://localhost:3000/',
+            url: '/recipe/filter',
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                exclude_filter: ["soy", "chicken", "kale"],
+                include_filter: []
+            }
+        };
+
+        const res = await axios(options);
+
+        // Check size
+        expect(Object.keys(res.data).length).toBe(30);
+    })
+
+    it("exclude filter test invalid", async() => {
+        const options = {
+            method: 'get',
+            baseURL: 'http://localhost:3000/',
+            url: '/recipe/filter',
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                exclude_filter: ["Jerome"],
+                include_filter: []
+            }
+        };
+
+        const res = await axios(options);
+
+        // Check size
+        expect(Object.keys(res.data).length).toBe(39);
+    })
+})
+
+describe("Include Exclude Test", () => {
+    it("1 include 1 exclude Test", async() => {
+        const options = {
+            method: 'get',
+            baseURL: 'http://localhost:3000/',
+            url: '/recipe/filter',
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                exclude_filter: ["butter"],
                 include_filter: ["bread"]
+            }
+        };
+
+        const res = await axios(options);
+
+        // Check size
+        expect(Object.keys(res.data).length).toBe(2);
+    })
+
+    it("1 include invalid exclude", async() => {
+        const options = {
+            method: 'get',
+            baseURL: 'http://localhost:3000/',
+            url: '/recipe/filter',
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                exclude_filter: ["soy"],
+                include_filter: ["bread"]
+            }
+        };
+
+        const res = await axios(options);
+
+        // Check size
+        expect(Object.keys(res.data).length).toBe(3);
+    })
+
+    it("2 include 1 exclude", async() => {
+        const options = {
+            method: 'get',
+            baseURL: 'http://localhost:3000/',
+            url: '/recipe/filter',
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                exclude_filter: ["beans"],
+                include_filter: ["tomato", "garlic"]
             }
         };
 
